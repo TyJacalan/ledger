a76338e430af2e41f48d426785d0595ff7543c70
ty <ty@bee-debian.home>
2024-02-22 18:47:25 +0800

    initial commit

---
+++ app/helpers/hobin/rail_helper.rb
@@ -0,0 +1,2 @@
+module Hobin::RailHelper
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  root "home#index"
+end
