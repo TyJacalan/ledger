1837b6ae584464d2d72e41b8b10ccb5719580ce4
Ty Jac <muzuuu@protonmail.com>
2024-02-23 18:54:17 +0800

    feat: add crud and set up views for tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index 
+    @tasks = current_user.tasks.all
+  end
+
+  def show
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def new
+    @task = current_user.tasks.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to root_path, notice: "Task was successfully created."
+    else
+      redirect_to root_path, alert: "Something went wrong!"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path, notice: "Task was successfully updated."
+    else
+      redirect_to root_path, alert: "Something went wrong!"
+    end
+  end
+
+  def destroy 
+    @task.destroy
+    redirect_to root_path, notice: "Task was successfully deleted."
+  end
+
+  private
+
+  def set_task
+    @task = current_User.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :due_date, :category_id)
+  end
+end
