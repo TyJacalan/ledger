bd4f301646aff158129a1bd920f8ce85965007d3
Ty Jac <muzuuu@protonmail.com>
2024-02-23 13:48:28 +0800

    feat: add routes for users, categories, and tasks

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "home#index"
+  
+  resources :users, only: [:edit, :update, :destroy] do
+    resources :categories
+    resources :tasks
+  end
+
 end
