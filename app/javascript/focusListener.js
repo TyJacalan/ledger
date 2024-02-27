4961182611fadcc2a55801c9801ac4001c76e7db
Ty Jac <muzuuu@protonmail.com>
2024-02-27 19:23:03 +0800

    feat: add hover arrow key list navigation functionality

--- app/javascript/focusListener.js
+++ app/javascript/focusListener.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     const homeBoxes = document.querySelectorAll(".home-box");
     
-    console.log(homeBoxes);
-
     homeBoxes.forEach(function(box, index) {
         box.addEventListener('focus', function(e) {
             const focusedElement = e.target;
-            console.log('Currently focused element:', focusedElement.tabIndex);
+            //console.log('Currently focused element:', focusedElement.tabIndex);
         });
     });
 });
