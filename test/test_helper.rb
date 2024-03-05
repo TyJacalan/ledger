1d037ca9538db14fdf1b5b33c255135088ba6999
Ty Jac <muzuuu@protonmail.com>
2024-03-05 19:41:11 +0800

    chore: add tests for Category model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,14 @@ module ActiveSupport
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+    fixtures :users, :categories, :tasks
 
     # Add more helper methods to be used by all tests here...
+  end
+end
+
+module SignInHelper
+  def sign_in_as(user)
+    post sign_in_url(email: user.email, password: user.password)
   end
 end
