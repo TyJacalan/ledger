61a200e3d5f7d80ce9f38b37996fd632543b9043
Ty Jac <muzuuu@protonmail.com>
2024-03-05 18:13:13 +0800

    fix: add user scope for category uniqueness validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
 end
