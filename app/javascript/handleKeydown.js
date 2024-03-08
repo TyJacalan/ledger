706b2137d95b53faaadbf32563165bd45869daea
Ty Jac <muzuuu@protonmail.com>
2024-03-08 16:51:36 +0800

    feat: add home box navigation

--- app/javascript/handleKeydown.js
+++ app/javascript/handleKeydown.js
@@ -12,5 +12,9 @@ function handleKeydown(){
 		if(e.key === "Enter" && App.dueDateListContainer.classList.contains("focused")){
 			handleTaskFilter(App.dueDateListContainer, "due_date");
 		}
+
+		if (e.ctrlKey && e.key === 'j' || e.ctrlKey && e.key === 'k' ) {
+			handleNavigateBoxes(e);
+		}
 	});
 }
