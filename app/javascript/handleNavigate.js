107939d677c5d158569ba40b3b4637a7b1436f4f
Ty Jac <muzuuu@protonmail.com>
2024-03-08 16:13:08 +0800

    fix: refactor navigator object

--- app/javascript/handleNavigate.js
+++ app/javascript/handleNavigate.js
@@ -1,28 +1,27 @@
-const Navigator = {
-	focusedIndex: -1,
-	previousItem: null,
-	list: null,
-	listItem: null,
+let focusedIndex =  -1;
+let previousItem = null;
+let list =  null;
+let listItem = null;
 
-	handleNavigate: function(container, listTagName, e) {
-		this.list = container.querySelector(listTagName);
+function handleNavigate(container, listTagName, e){
+	list = container.querySelector(listTagName);
 
-		if ((e.key === "ArrowUp" || e.key === "ArrowLeft") && this.focusedIndex > 0) {
-			e.preventDefault();
-			this.updateFocus(-1);
-		} else if ((e.key === "ArrowDown" || e.key === "ArrowRight") && this.focusedIndex < this.list.children.length - 1) {
-			e.preventDefault();
-			this.updateFocus(1);
-		}
-	},
-	updateFocus: function(change) {
-		if (this.previousItem) {
-			this.previousItem.classList.remove('active');
-		}
-		this.focusedIndex += change;
-		this.listItem = this.list.children[this.focusedIndex];
-		this.listItem.classList.add('active');
-		this.previousItem = this.listItem;
+	if((e.key === "ArrowUp" || e.key === "ArrowLeft") && focusedIndex > 0){
+		e.preventDefault();
+		updateFocus(-1);
+	} else if ((e.key === "ArrowDown" || e.key === "ArrowRight") && focusedIndex < list.children.length -1) {
+		e.preventDefault();
+		updateFocus(1);
 	}
-};
+}
 
+function updateFocus(change){
+	if(previousItem != null){
+		previousItem.classList.remove('active');
+	}
+
+	focusedIndex += change;
+	listItem = list.children[focusedIndex];
+	listItem.classList.add('active');
+	previousItem = listItem;
+}
