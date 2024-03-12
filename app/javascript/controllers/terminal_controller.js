859a62a59535813c05724d34761184b66169310c
Ty Jac <muzuuu@protonmail.com>
2024-03-12 18:30:32 +0800

    feat: add command form logic

--- app/javascript/controllers/terminal_controller.js
+++ app/javascript/controllers/terminal_controller.js
@@ -1,20 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "ui" ]
+    static targets = [ "ui", "commandField"]
 
     initialize() {
         this.index = 0
         this.showCurrentUi()
-        document.addEventListener("keydown", e => this.handleKeyDown(e))
+        document.addEventListener("keydown", this.handleKeyDown)
     }
 
-    handleKeyDown(e) {
-        if(e.ctrlKey && e.key === "c"){
-            this.showCommandForm()
-        }
-    }
-    
     showHelper() {
         this.index = 0
         this.showCurrentUi()
@@ -30,9 +24,44 @@ export default class extends Controller {
         this.showCurrentUi()
     }
 
+    showTaskForm() {
+        this.index = 3
+        this.showCurrentUi()
+    }
+
     showCurrentUi() {
         this.uiTargets.forEach((element, index) => {
             element.hidden = index !== this.index
+            this.currentElement = element.children[0]
+
+            if(index === this.index) {
+                this.currentElement.focus()
+                this.currentElement.addEventListener("blur", this.handleBlur)
+            } else {
+                this.currentElement.removeEventListener("blur", this.handleBlur)
+            }
         })
+    }
+
+    handleKeyDown = (e) => {
+        if(e.ctrlKey && e.key === "c"){
+            this.showCommandForm()
+        } else if (e.key === "Enter" && this.index === 1) {
+            const command = this.commandFieldTarget.value.trim()
+            this.commandFieldTarget.value = ""
+
+            switch (command) {
+                case ":C":
+                    this.showCategoryForm()
+                    break;
+                case ":T":
+                    this.showTaskForm()
+                    break;
+            }
+        }
+    }
+
+    handleBlur = () => {
+        this.showHelper()
     }
 }
---
+++ app/views/home/terminal/_category_form.html.erb
@@ -0,0 +1,1 @@
+<h1>Category Form</h1>
