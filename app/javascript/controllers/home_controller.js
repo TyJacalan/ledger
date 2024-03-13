5b3793c867d4df1bd2d5f318b56fb533c3ceaf74
Ty Jac <muzuuu@protonmail.com>
2024-03-13 19:51:29 +0800

    refactor: implement home box partials

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -1,6 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static targets = ["homeBox"]
+
+    connect() {
+        this.index = 0;
+        this.focusCurrentBox()
+    }
+
+    nextBox() {
+        this.indexValue++
+        this.focusCurrentBox()
+    }
+
+    prevBox() {
+        this.indexValue--
+        this.focusCurrentBox()
+    }
+
+    focusCurrentBox() {
+        this.homeBoxTargets.forEach((element, index) => {
+            element.focus() = index != this.indexValue
+        })
+    }
+
     showCommandForm() {
         window.dispatchEvent(new CustomEvent("show-command-form"))
     }
