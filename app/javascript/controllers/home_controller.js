bc08616c29468527e971df7f103917d8daee40ef
Ty Jac <muzuuu@protonmail.com>
2024-03-13 18:08:17 +0800

    feat: add toast setup

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -1,5 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    showCommandForm() {
+        window.dispatchEvent(new CustomEvent("show-command-form"))
+    }
 
+    showToast() {
+        console.log("showing toast")
+        window.dispatchEvent(new CustomEvent("show-toast"))
+    }
 }
