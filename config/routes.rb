d4a81cab3e560b3660228da5fe0c0c624e89830f
Ty Jac <muzuuu@protonmail.com>
2024-03-16 12:02:52 +0800

    refactor: clean category and task routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
   
-  resources :categories
-  resources :tasks
+  resources :categories, except: [:new, :index ]
+  resources :tasks, except: [:new, :index ]
 
   root "home#index"
 end
